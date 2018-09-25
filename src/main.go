395b5e7cb8b09b2d82ba67cae9f86e81a5f1be1a
pan <pan@MacBook-Pro.local>
2018-09-25 17:14:43 +0800

    package test

--- src/main.go
+++ src/main.go
@@ -5,15 +5,16 @@ import (
 	"net/http"
 	"fmt"
 	"github.com/gorilla/mux"
+	"handler"
 )
 /**
-git test
+git test 
 **/
 func main(){
 	fmt.Println("test")
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/test",TestHandler/*1*/).Methods("GET")
+	router.HandleFunc("/test",maind.TestHandler1).Methods("GET")
 
 	var address = fmt.Sprintf("%s:%d","localhost",8080)
 	error := http.ListenAndServe(address,router)
@@ -23,7 +24,9 @@ func main(){
 	fmt.Println("Server end")
 }
 
+/*
 func TestHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	w.Write([]byte("test"))
 }
+*/
